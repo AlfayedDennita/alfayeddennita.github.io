125dfa2a3ab4b5c226b2ecc6c6dfa9faf866ef83
Alfayed Dennita <32590788+AlfayedDennita@users.noreply.github.com>
2022-04-16 15:41:56 +0700

    Delete portfolio directory

--- portfolio/slicing/stack-404-page/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.querySelector('#hamburger-button');
-const navigationContainer = document.querySelector('header nav');
-
-hamburgerButton.addEventListener('click', () => {
-  navigationContainer.classList.toggle('open');
-});
--- portfolio/slicing/yre-travel/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.querySelector('#hero-navigation button');
-const navigationMenu = document.querySelector('#hero-navigation ul');
-
-hamburgerButton.addEventListener('click', () => {
-  navigationMenu.classList.toggle('open');
-});
